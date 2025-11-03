b04f43d03988d1d6c163cc69f993ca6ce96c4902
eliseo-lopez-bravo <eliseo.lopez.007@gmail.com>
2025-11-03 23:12:25 +0000

    addressing providers error

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32.0"
+    }
+  }
+}
+
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,21 +1,18 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.32.0"
-    }
     oci = {
       source  = "oracle/oci"
       version = ">= 5.0.0"
     }
-  required_version = ">= 1.4.0"
   }
 }
 
 provider "oci" {
-  tenancy_ocid        = var.tenancy_ocid
-  user_ocid           = var.user_ocid
-  fingerprint         = var.fingerprint
-  private_key_path    = var.private_key_path
-  region              = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
